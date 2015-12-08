bbdfda2bb6f43fe1b7f3c26f1721f06b0ceb5899
Osama B <osama.benhabara@ungapped.com>
2015-12-08 10:40:59 +0100

    Add the Jetshop-controller that were supposed to come with the last commit

---
+++ Integrations/Controllers/JetshopController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using System.Web.Http;
+
+namespace Integrations.Controllers
+{
+    [RoutePrefix("Jetshop")]
+    public class JetshopController : ApiController
+    {
+        [Route("Products")]
+        [HttpGet]
+        public IHttpActionResult GetProducts([FromUri] string domain)
+        {
+            var URL = $"http://{domain}/Services/Rest/v1/json/products";
+            string jsonString;
+            using (var wc = new WebClient())
+            {
+
+                jsonString = wc.DownloadString(URL);
+            }
+            dynamic cleanJson = JsonConvert.DeserializeObject(jsonString);
+            return Ok(cleanJson);
+        }
+    }
+}
